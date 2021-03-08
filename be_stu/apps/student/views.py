de03cef7cc7a9e2af2b0aecc8ed4c6c8a3ec9896
AnacondaFeng <fengyu987@163.com>
2021-03-08 22:03:18 +0800

    03082203

--- be_stu/apps/student/views.py
+++ be_stu/apps/student/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# 引入Student类
+from .models import Student
+# 引入JsonResponse模块
+from django.http import JsonResponse
+
+# 定义方法获取全部学生信息
+def get_students(request):
+    try:
+        # obj_students = Student.object.all().values()
+        obj_students = Student.objects.all().values()
+        students = list(obj_students)
+        return JsonResponse({'code':1, 'data':students})
+    except Exception as e:
+        return JsonResponse({'code':0, 'msg':"异常："+str(e)})
